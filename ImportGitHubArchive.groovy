83d02ec73db57763c3c3ed980deb2d351e0fb99e
Vadas Gintautas <vadasg@gmail.com>
2012-10-02 17:28:45 -0400

    new branch with more complicated schema.  still debugging on this branch

--- ImportGitHubArchive.groovy
+++ ImportGitHubArchive.groovy
@@ -1,20 +1,23 @@
 import groovy.json.JsonSlurper
 import com.thinkaurelius.titan.core.*
 
+
 start = System.currentTimeMillis() 
 last = start
-def config = [ "cache_type":"none" ] 
+def config = [ 'cache_type':'none' ] 
 
 graph = TitanFactory.open('../../scratch/gha')
 graph.createKeyIndex('name',Vertex.class)
-BatchGraph bgraph = new BatchGraph(graph, BatchGraph.IdType.STRING, 10000)
+BatchGraph bgraph = new BatchGraph(graph, BatchGraph.IdType.OBJECT, 10000)
 slurper = new JsonSlurper()
 
 count = 0
 
+
 //written as function instead of closure due to recursion
 def safePropertyAdder(currentObject, propertyMap){
     for (pair in propertyMap) {
+        println pair
         if (pair.value == null) return
         try{
             if (pair.value.getClass().equals(java.util.HashMap)){
@@ -27,54 +30,121 @@ def safePropertyAdder(currentObject, propertyMap){
         }
         catch(ScriptException){  
             //needed if the key is "id" or "label" or some other reserved word
-            currentObject.setProperty(pair.key+"_",pair.value)
+            currentObject.setProperty(pair.key+'_',pair.value)
         }
     }
 }
 
+def addVertex = { name, properties ->
 
-def addEdges = { line -> 
-    result = slurper.parseText(line)
-    result = slurper.parseText(line)
-    if (result.type == 'GistEvent') return
+            vertex=bgraph.getVertex(name)
+            if (vertex==null) {
+                vertex=bgraph.addVertex(name)
+                safePropertyAdder(vertex,properties)
+            }
+            return vertex
+}
 
-    //println result.actor
-    
-    //out vertex is actor
-    outVertex=bgraph.getVertex(result.actor)
-    if (outVertex==null) {
-        outVertex=bgraph.addVertex(result.actor)
-        safePropertyAdder(outVertex, result.actor_attributes) 
+def addEdge = {inVertex, outVertex, name, properties->
+
+        println 'just checking at edge 1'
+        edge = bgraph.addEdge(null,outVertex,inVertex,name)
+        println 'just checking at edge 2'
+        safePropertyAdder(edge,properties)
+        return edge
+}
+
+
+def loader = { line -> 
+    s = slurper.parseText(line)
+    s = slurper.parseText(line)
+    println s.actor
+    println s.type
+    //
+    //out vertex is always a user
+    //name is username or login
+    outVertex=addVertex(s.actor,['actor':s.actor]+s.actor_attributes)
+    s.remove('actor')
+    s.remove('actor_attributes')
+
+    switch (s.type.toString()){
+
+        case 'GistEvent':
+
+            inVertex = addVertex(s.payload.name, s.remove('payload'))
+            addEdge(outVertex,inVertex,'created',s)
+            break
+
+        case 'FollowEvent':
+            
+            inVertex = addVertex(s.payload.target.login, s.remove('payload').remove('target'))
+            addEdge(outVertex,inVertex,'followed',s)
+            break
+
+        case ['MemberEvent','TeamAddEvent']:
+            
+            inVertex1 = addVertex(s.payload.target.login, s.remove('payload').remove('target'))
+            inVertex2 = addVertex(s.repository.name,s.remove('repository'))
+            addEdge(outVertex,inVertex1,'added',s)
+            addEdge(inVertex1,inVertex2,'to',s)
+            break
+
+        case 'CommitCommentEvent':
+            return
+            inVertex1 = addVertex(s.payload.comment_id, s.remove('payload'))
+            inVertex2 = addVertex(s.repository.name,s.remove('repository'))
+            println 'just checking'
+            println s
+            addEdge(outVertex,inVertex1,'created',s)
+            //addEdge(inVertex1,inVertex2,'on',s)
+            break
+
+        case 'IssuesEvent':
+            
+            inVertex1 = addVertex(s.payload.issue,s.payload)
+            inVertex2 = addVertex(s.repository.name,s.remove('repository'))
+            addEdge(outVertex,inVertex1,'created',s)
+            addEdge(inVertex1,inVertex2,'on',s)
+            break
+
+        case 'IssuesCommentEvent':
+            
+            inVertex1 = addVertex(s.payload.comment_id, [:])
+            inVertex2 = addVertex(s.payload.issue_id,[:])
+            inVertex3 = addVertex(s.repository.name,s.remove('repository'))
+            addEdge(outVertex,inVertex1,'created',s)
+            addEdge(inVertex1,inVertex2,'on',s)
+            addEdge(inVertex2,inVertex3,'on',s)
+            break
+
+        case 'PullRequestReviewCommentEvent':
+            
+            inVertex1 = addVertex(s.payload.comment.comment_id, s.remove('payload'))
+            inVertex2 = addVertex(s.repository.name,s.remove('repository'))
+            addEdge(outVertex,inVertex1,'created',s)
+            addEdge(inVertex1,inVertex2,'on',s)
+            break
+
+        case [ 'CreateEvent', 'WatchEvent', 'DownloadEvent', 'DeleteEvent', 'ForkEvent', 'ForkApplyEvent', 'GollumEvent', 'PublicEvent', 'PullRequestEvent' ]:
+
+            edgeNameMap = [
+                'CreateEvent':'created',
+                'WatchEvent':'watched',
+                'DownloadEvent':'downloaded',
+                'DeleteEvent':'deleted',
+                'ForkEvent':'forked',
+                'ForkApplyEvent':'appliedForkTo',
+                'GollumEvent':'editedWikiOf',
+                'PublicEvent':'madePublic',
+                'PullRequestEvent':'pullRequested',
+                'PushEvent':'pushed'
+            ]
+            inVertex = addVertex(s.repository.name,s.remove('repository'))
+            addEdge = addEdge(outVertex,inVertex,edgeNameMap[s.type],s)
+            break
+        
+
     }
-    outVertex.setProperty("actor",result.actor)
-    //println outVertex
-
-    if (result.type == 'FollowEvent') {
-        //in vertex is another user
-        inVertex=bgraph.getVertex(result.payload.target.login)
-        if (inVertex==null) {
-            inVertex=bgraph.addVertex(result.payload.target.login)
-        }
-        safePropertyAdder(inVertex,result.payload.target)
-        //println inVertex
-        result.remove("payload")
-    }
-    else {
-        //in vertex is repository
-        inVertex=bgraph.getVertex(result.repository.name)
-        if (inVertex==null) {
-            inVertex=bgraph.addVertex(result.repository.name)
-            safePropertyAdder(inVertex,result.repository)
-        }
-        //println inVertex
-        result.remove("repository")
-    }
-        result.remove("actor")
-        result.remove("actor_attributes")
-        Edge edge = bgraph.addEdge(null,outVertex,inVertex,result.type)
-        //println edge
-        safePropertyAdder(edge,result)
-
     count = count + 1
     if (count % 1000000 == 0 ) { 
         now = System.currentTimeMillis()
@@ -84,6 +154,9 @@ def addEdges = { line ->
     }
     
 }
+
+
+
 
 folder = '../../scratch/githubarchive'
 baseDir = new File(folder)
@@ -100,7 +173,7 @@ For now, assume that input files contain one json entry per line.  Some of the e
 //}
 
 
-myFile = new File('../../scratch/githubarchive/2012-09-27-8.json').eachLine {line ->addEdges(line)}
+myFile = new File('../../scratch/githubarchive/2012-09-27-8.json').eachLine {line ->loader(line)}
 now = System.currentTimeMillis()  
 
 graph.shutdown()
