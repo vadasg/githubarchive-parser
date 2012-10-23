bb7ae0f4846a92592ced9a45d4b50250412e1c89
Vadas Gintautas <vadasg@gmail.com>
2012-10-23 18:35:27 -0400

    changed importer to use preformatted data

--- ImportGitHubArchive.groovy
+++ ImportGitHubArchive.groovy
@@ -1,8 +1,9 @@
 import groovy.json.JsonSlurper
+import groovy.json.JsonBuilder
 import com.thinkaurelius.titan.core.*
 
 /** 
- * Reads in gzipped githubarchive json files in a specified directory
+ * Reads in uncompressed githubarchive json files in a specified directory
  * and loads into Titan graph
  * @author Vadas Gintautas
  *
@@ -11,33 +12,49 @@ import com.thinkaurelius.titan.core.*
  */
 
 
-debug = false
-verbose = false
-useHBase = true  //if false, use BerkleyDB instead
-
-//get inputFolder as command line argument
-try {
-    inputFolder = a1
-}
-catch (MissingPropertyException) {
-    throw new IllegalArgumentException('\n\nusage: gremlin -e ImportGitHubArchive.groovy <inputFolder>\n')
-}
-
+useHBase = false  //if false, use BerkleyDB instead
 graphLocation = '/tmp/debug_graph' //only for BerkleyDB
 
 
+//get inputFolder as command line argument
+try {
+    inputVerticesFile = a1
+    inputEdgesFile = a2
+}
+catch (MissingPropertyException) {
+    throw new IllegalArgumentException('\n\nusage: gremlin -e ImportGitHubArchive.groovy <inputVertices> <inputEdges>\n')
+}
 
 
 
 slurper = new JsonSlurper()
-edgeCount = 0
+start = System.currentTimeMillis() 
+last  = System.currentTimeMillis() 
 vertexCount = 0
-eventCount = 0
+edgeCount = 0
 
-/**
- * Written as function instead of closure due to recursion
- */
-def safePropertyAdder(currentObject, propertyMap){
+def counter= {type ->
+    if (type.equals('vertex')) vertexCount = vertexCount + 1
+    else if (type.equals('edge')) edgeCount = edgeCount + 1
+
+
+
+    if ((vertexCount > 0) && (vertexCount % 1000000).equals(0)){
+        now = System.currentTimeMillis()
+        elapsed = ((now - last)/1000.0)
+        last = now
+        println vertexCount + ' vertices in ' + elapsed + ' seconds'
+    }
+    if ((edgeCount > 0) && (edgeCount  % 1000000 ).equals(0)){ 
+        now = System.currentTimeMillis()
+        elapsed = ((now - last)/1000.0)
+        last = now
+        println edgeCount + ' edges in ' + elapsed + ' seconds'
+    }
+}
+
+
+def safePropertyAdder = {currentObject, propertyMap ->
     for (pair in propertyMap) {
         if (pair.value == null) return
         if (pair.key in ['type','name','id','label']){
@@ -48,282 +65,49 @@ def safePropertyAdder(currentObject, propertyMap){
 }
 
 
-def vertexAdder = {g,  name, type, properties ->
+def vertexAdder = {g, s, line ->
+
+    vertexId = line.substring(0, line.indexOf('\t'))
+    propertiesString = line.substring(line.indexOf('\t')+1,line.size())
+    name = vertexId.reverse().substring(vertexId.reverse().indexOf('_')+1,vertexId.size()).reverse()
+    type = vertexId.reverse().substring(0,vertexId.reverse().indexOf('_')).reverse()
+    if (propertiesString.equals('null')) propertiesString = '{}'
+    properties = s.parseText(propertiesString)
+
+
     if (name==null) throw new IllegalArgumentException('Name cannot be null')
-    if (name==null) throw new IllegalArgumentException('Type cannot be null')
+    if (type==null) throw new IllegalArgumentException('Type cannot be null')
 
-    name = name.toString()
-    vertexId = name+'_'+type
-    vertex=g.getVertex(vertexId)
+    vertex=g.addVertex(vertexId)
+    safePropertyAdder(vertex,properties)
+    vertex.setProperty('name',name)
+    vertex.setProperty('type',type)
+    counter('vertex')
     
-    if ((vertex==null) || (!vertex.getProperty('type').equals(type))){
-        vertex=g.addVertex(vertexId)
-        vertexCount = vertexCount + 1
-        safePropertyAdder(vertex,properties)
-        vertex.setProperty('name',name)
-        vertex.setProperty('type',type)
-    }
-    return vertex
 }
 
 
-def edgeAdder = {g,outVertex, inVertex, label, properties->
-    if (label==null) throw new IllegalArgumentException('Name cannot be null')
+def edgeAdder = {g, s, line ->
+    firstTab = line.indexOf('\t')
+    secondTab = line.indexOf('\t',firstTab+1)
+    thirdTab = line.indexOf('\t',secondTab+1)
+
+    outVertex = line.substring(0,firstTab)
+    inVertex = line.substring(firstTab+1,secondTab)
+    label = line.substring(secondTab+1,thirdTab)
+    propertiesString = line.substring(thirdTab+1,line.size())
+    if (propertiesString.equals('null')) propertiesString = '{}'
+    properties = s.parseText(propertiesString)
+
+    if (label==null) throw new IllegalArgumentException('Label cannot be null')
     
-    edge = g.addEdge(null,outVertex,inVertex,label)
-    edgeCount = edgeCount + 1
+    edge = g.addEdge(null,g.getVertex(outVertex),g.getVertex(inVertex),label)
     safePropertyAdder(edge,properties)
-    return edge
-}
-
-
-def loader = {g, line -> 
-    s = slurper.parseText(line)
-    if (s.actor == null) return
-
-    if (verbose) {
-        println s
-        println s.actor
-        println s.type
-    }
-    
-    /**
-     * Out vertex is always a user.
-     * Occasionally actor_attributes is missing so we allow for this.
-     */
-    actorProperties = ['actor':s.actor]
-    if (s.actor_attributes != null) actorProperties = actorProperties +  s.remove('actor_attributes')
-    lastVertex=vertexAdder(g, s.remove('actor'),'User',actorProperties)
-
-
-    try {
-        repoOrg = s.repository.organization
-    }
-    catch (NullPointerException) {
-        repoOrg = null
-    }
-            
-    edgeLabelMap = [
-        'CreateEvent':'created',
-        'WatchEvent':'watched',
-        'DownloadEvent':'downloaded',
-        'DeleteEvent':'deleted',
-        'ForkEvent':'forked',
-        'ForkApplyEvent':'appliedForkTo',
-        'PublicEvent':'madePublic',
-        'PullRequestEvent':'pullRequested',
-    ]
-
-    switch (s.type){
-
-        /**
-         * GistEvent: User created Gist.
-         */
-        case 'GistEvent':
-        
-            vertexNames = [s.payload.name]
-            vertexTypes = ['Gist']
-            vertexProperties = [s.remove('payload')]
-            edgeLabels = ['created']
-            edgeProperties = [s]
-            break
-        
-        /**
-         * FollowEvent: User followed User.
-         */
-        case 'FollowEvent':
-            
-            vertexNames = [s.payload.target.login]
-            vertexTypes = ['User']
-            vertexProperties = [s.remove('payload').remove('target')]
-            edgeLabels = ['followed']
-            edgeProperties = [s]
-            break
-
-        /**
-         * MemberEvent, TeamAddEvent: User added User to Repository.
-         * No TeamAddEvents observed in entire archive to date.
-         * Occasionally repository is missing (here and following cases).
-         * If so, ignore the event.
-         */
-        case ['MemberEvent','TeamAddEvent']:
-            if (s.repository == null) return
-
-            vertexNames = [s.payload.member.login,s.repository.name]
-            vertexTypes = ['User','Repository']
-            vertexProperties = [s.remove('payload').remove('target'),s.remove('repository')]
-            edgeLabels = ['added','to']
-            edgeProperties = [s,s]
-            break
-
-        /**
-         * CommitCommentEvent: User created Comment on Repository.
-         */
-        case 'CommitCommentEvent':
-            if (s.repository == null) return
-            
-            vertexNames = [s.payload.comment_id,s.repository.name]
-            vertexTypes = ['Comment','Repository']
-            vertexProperties = [s.remove('payload'),s.remove('repository')]
-            edgeLabels = ['created','on']
-            edgeProperties = [s,s]
-            break
-
-        /**
-         * IssuesEvent: User created Issue on Repository.
-         */
-        case 'IssuesEvent':
-            if (s.repository == null) return
-
-            vertexNames = [s.payload.issue,s.repository.name]
-            vertexTypes = ['Issue','Repository']
-            vertexProperties = [s.remove('payload'),s.remove('repository')]
-            edgeLabels = ['created','on']
-            edgeProperties = [s,s]
-
-            break
-        
-        /**
-         * GollumEvent: User edited WikiPage of Repository.
-         */
-        case 'GollumEvent':
-            if (s.repository == null) return
-
-            pageNames = []
-            pageProperties = []
-            pageTypes = []
-            pageEdgeNames = []
-            pageEdgeProperties = []
-
-            for (p in s.payload.pages){
-                pageNames.add(p.remove('html_url'))
-                pageProperties.add(p)
-                pageTypes.add('WikiPage')
-                pageEdgeNames.add('edited')
-                pageEdgeProperties.add([:])
-            }
-            
-
-            vertexNames = [pageNames,s.repository.name]
-            vertexTypes = [pageTypes,'Repository']
-            vertexProperties = [pageProperties, s.remove('repository')]
-            edgeLabels = [pageEdgeNames,'of']
-            edgeProperties = [pageEdgeProperties,s]
-
-            break
-
-        /**
-         * PushEvent: User pushed commit to Repository.
-         */
-        case 'PushEvent':
-            if (s.repository == null) return
-            if (s.payload.shas.size() == 0) return
-
-            pageNames = []
-            pageProperties = []
-            pageTypes = []
-            pageEdgeNames = []
-            pageEdgeProperties = []
-
-            for (p in s.payload.shas){
-                pageNames.add(p[0])
-                pageProperties.add(['payload':p.drop(1)])
-                pageTypes.add('Commit')
-                pageEdgeNames.add('pushed')
-                pageEdgeProperties.add(['created_at':s.created_at,'public':s.public,'url':s.url])
-            }
-
-            s.remove('payload')
-            
-
-            vertexNames = [pageNames,s.repository.name]
-            vertexTypes = [pageTypes,'Repository']
-            vertexProperties = [pageProperties, s.remove('repository')]
-            edgeLabels = [pageEdgeNames,'to']
-            edgeProperties = [pageEdgeProperties,s]
-
-            break
-        
-        
-        /**
-         * IssuesCommentEvent: User created Comment on Issue on Repository.
-         */
-        case 'IssueCommentEvent':
-            if (s.repository == null) return
-            
-            vertexNames = [s.payload.comment_id,s.payload.issue_id,s.repository.name]
-            vertexTypes = ['Comment','Issue','Repository']
-            vertexProperties = [[:],[:],s.remove('repository')]
-            edgeLabels = ['created','on','on']
-            edgeProperties = [s,s,s]
-            break
-
-        /**
-         * PullRequestReviewCommentEvent: User created Comment on Repository.
-         */
-        case 'PullRequestReviewCommentEvent':
-            if (s.repository == null) return
-            
-            vertexNames = [s.payload.comment.commit_id,s.repository.name]
-            vertexTypes = ['Comment','Repository']
-            vertexProperties = [s.remove('payload'),s.remove('repository')]
-            edgeLabels = ['created','on']
-            edgeProperties = [s,s]
-            break
-
-
-        /**
-         * All other valid cases:  User (edgeLabel) Repository
-         */
-        case edgeLabelMap.keySet() as List:
-            if (s.repository == null) return
-
-            vertexNames = [s.repository.name]
-            vertexTypes = ['Repository']
-            vertexProperties = [s.remove('repository')]
-            edgeLabels = [edgeLabelMap[s.type]]
-            edgeProperties = [s]
-            break
-        
-
-    }
-
-    eventCount = eventCount + 1
-    if (eventCount % 1000000 == 0 ) { 
-        now = System.currentTimeMillis()
-        sec = (now - last)/1000.0
-        println sec.toString() +  ' s for ' + eventCount.toString()
-        last = now
-    }
-
-    for (i in 0..vertexNames.size()-1) {
-
-        if ((vertexTypes[i] == 'Repository') && (repoOrg != null)){
-            nextVertex = vertexAdder(g, vertexNames[i],vertexTypes[i],vertexProperties[i])
-            orgVertex = vertexAdder(g,repoOrg,'Organization',[:])
-            edgeAdder(g,orgVertex,nextVertex,'owns',[:])
-        }
-
-        if (vertexNames[i].getClass() == java.util.ArrayList){
-            endVertex = vertexAdder(g, vertexNames[i+1],vertexTypes[i+1],vertexProperties[i+1])
-            for (j in 0..vertexNames[i].size()-1){
-                midVertex = vertexAdder(g, vertexNames[i][j],vertexTypes[i][j],vertexProperties[i][j])
-                edgeAdder(g,lastVertex,midVertex,edgeLabels[i][j],edgeProperties[i][j])
-                edgeAdder(g,midVertex,endVertex,edgeLabels[i+1],edgeProperties[i+1])
-            }
-            return
-        } else{
-            nextVertex = vertexAdder(g, vertexNames[i],vertexTypes[i],vertexProperties[i])
-            edgeAdder(g,lastVertex,nextVertex,edgeLabels[i],edgeProperties[i])
-            lastVertex = nextVertex
-        }
-    }
+    counter('edge')
 }
 
 
 
-
-start = System.currentTimeMillis() 
 last = start
 def config = [ 'cache_type':'none' ] 
 
@@ -336,35 +120,14 @@ if (useHBase){
 }else graph = TitanFactory.open(graphLocation)
 
 graph.createKeyIndex('name',Vertex.class)
-BatchGraph bgraph = new BatchGraph(graph, BatchGraph.IdType.STRING, 5000)
+BatchGraph bgraph = new BatchGraph(graph, BatchGraph.IdType.STRING, 20000)
+bgraph.setLoadingFromScratch(true)
 
+println 'Loading vertices'
+myFile = new File(inputVerticesFile).eachLine {line ->vertexAdder(bgraph, slurper, line)}
+println 'Loading edges'
+myFile = new File(inputEdgesFile).eachLine {line ->edgeAdder(bgraph, slurper, line)}
 
-baseDir = new File(inputFolder)
-fileList = baseDir.listFiles()
-fileList.sort()
-
-
-if (debug) {
-    testFile = inputFolder + '/2012-04-17-8.json.gz'
-    fileList = [testFile]
-}
-
-
-/**
- * could rewrite this so it streams the input but this works for now
- * also loading from temporary json file facilitates debugging
- */
-
-aCounter = 0;
-for (file in fileList){
-    fileName = file.toString()
-    System.out.println('[' + aCounter++ + ':' + fileList.size() + '] ' + fileName)
-
-    command = 'ruby FixGitHubArchiveDelimiters.rb ' + fileName + ' /tmp/temp.json'
-    process = command.execute()
-    process.waitFor()
-    myFile = new File('/tmp/temp.json').eachLine {line ->loader(bgraph, line)}
-}
 graph.shutdown()
 
 
@@ -372,7 +135,6 @@ graph.shutdown()
 now = System.currentTimeMillis()  
 elapsed =  ((now - start)/1000.0)
 println 'Done.  Statistics:'
-println eventCount + ' events'
 println vertexCount + ' vertices'
 println edgeCount + ' edges'
 println elapsed + ' seconds elapsed'
